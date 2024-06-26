6b552efd307659a57def9dbf1ce474c7273bad8a
lombo-cmyk <srsrsrew@gmail.com>
2024-06-26 21:46:21 +0100

    schedule led job

--- illumination/illumination/illumination.py
+++ illumination/illumination/illumination.py
@@ -1,16 +1,20 @@
+import schedule
 from time import sleep
 from gpiozero import LED
 
 from illumination.logger import logger
 
-
+    
 led = LED(14)
 
 
+def led_job():
+    logger.info(f"Changing led state.")
+    led.toggle()
+
+
 def run():
+    schedule.every(10).seconds.do(led_job)
     while True:
-        led.on()
-        logger.info("going through the loop")
-        sleep(1)
-        led.off()
+        schedule.run_pending()
         sleep(1)
