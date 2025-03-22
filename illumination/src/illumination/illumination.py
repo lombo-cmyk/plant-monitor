37c267aa97ef74bda87109eb27d9a5f3d2896515
Lukasz Kania <srsrsrew@gmail.com>
2025-03-22 20:25:36 +0100

    #2 Move illumination to BaseService

--- illumination/src/illumination/illumination.py
+++ illumination/src/illumination/illumination.py
@@ -1,39 +1,11 @@
-from functools import partial
-from threading import Thread
-from time import sleep
-
-import schedule
-from gpiozero import LED
+from illumination.service import Service
 
 from plant_common.logger import get_logger
-from plant_common.model import LedState
-from plant_common.mqtt import MqttClient
+
 
 logger = get_logger("illumination")
-led = LED(14)
-LED_ON_TIME_S = 5 * 60
-
-
-def led_job(client: MqttClient):
-    logger.info(f"Turning LED ON for {LED_ON_TIME_S}s.")
-    led.on()
-    client.publish("led/state", LedState.build(True))
-
-    def turn_led_off():
-        sleep(LED_ON_TIME_S)
-        logger.info("Turning LED OFF.")
-        led.off()
-        client.publish("led/state", LedState.build(False))
-
-    th = Thread(target=turn_led_off)
-    th.start()
 
 
 def run():
-    mqtt_client = MqttClient(logger, client_id="illumination", transport="websockets")
-    mqtt_client.connect(host="mosquitto-broker", port=9001)
-    fun = partial(led_job, client=mqtt_client)
-    schedule.every().hour.at(":01").do(fun)
-    while True:
-        schedule.run_pending()
-        sleep(1)
+    service = Service("illumination", logger)
+    service.run()
