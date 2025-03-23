dc204330178dc6e7d1940d63b8bcc3d0af6c3ee0
Lukasz Kania <srsrsrew@gmail.com>
2025-03-23 11:22:21 +0100

    #11 Gracefully shutdown all containers

--- plant_common/tests/test_service.py
+++ plant_common/tests/test_service.py
@@ -14,19 +14,21 @@ def test_run_not_implemented():
 
 
 @patch("plant_common.service.schedule")
-@patch("plant_common.service.sleep", side_effect=StopIteration)
+@patch("plant_common.service.sleep")
 def test_run(mock_sleep, mock_schedule):
     client = MagicMock()
     service = BaseService(name="test", logger=MagicMock(), client=client)
     service._setup_mqtt = MagicMock()
     service._setup_scheduled_jobs = MagicMock()
+    service.shutdown = MagicMock()
+    service.shutdown.__bool__ = MagicMock(side_effect=[False, True])
 
-    with pytest.raises(StopIteration):
-        service.run()
+    service.run()
 
     service._setup_mqtt.assert_called_once()
     service._setup_scheduled_jobs.assert_called_once()
-    mock_schedule.run_pending.assert_called_once()
+    mock_sleep.assert_called_once()
+    assert mock_schedule.run_pending.call_count == 2
 
 
 def test_setup_mqtt():
