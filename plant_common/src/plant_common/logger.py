24dfc4813e37ac1d42fa3fd781723bdb99eea44e
lombo-cmyk <61498393+lombo-cmyk@users.noreply.github.com>
2025-03-24 12:36:44 +0100

    Fix logger (#23)

--- plant_common/src/plant_common/logger.py
+++ plant_common/src/plant_common/logger.py
@@ -19,8 +19,6 @@ def get_logger(name: str):
     )
     timedRotatingHandler.setFormatter(logFormatter)
 
-    timedRotatingHandler.doRollover()
-
     logger.addHandler(timedRotatingHandler)
     logger.addHandler(consoleHandler)
 
