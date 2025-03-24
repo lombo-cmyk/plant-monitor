ce095abc5655a7dcdf1c69a1d2df51d0e96ed82d
Lukasz Kania <srsrsrew@gmail.com>
2025-03-24 11:10:42 +0100

    Check make tests results

--- build_helpers/make-tests.sh
+++ build_helpers/make-tests.sh
@@ -2,8 +2,17 @@
 
 source venv/bin/activate
 
+results=()
 for w in plant_common camera diagnostics illumination
 do
   echo "Running $w tests"
   pytest "./$w/"
+  results+=("$?")
+done
+
+for res in "${results[@]}"; do
+  if [[ $res -ne 0 ]]; then
+    echo "One or more tests failed"
+    exit 1
+  fi
 done
