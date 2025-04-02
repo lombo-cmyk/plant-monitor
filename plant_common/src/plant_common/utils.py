e81907425457e4d93e22bafeaf3ed7e00a30475f
Lukasz Kania <srsrsrew@gmail.com>
2025-04-02 15:10:58 +0200

    Reduce uptime resolution to 1s

--- plant_common/src/plant_common/utils.py
+++ plant_common/src/plant_common/utils.py
@@ -6,5 +6,5 @@ import psutil
 
 def get_uptime() -> timedelta:
     p = psutil.Process(os.getpid())
-    creation_time = datetime.fromtimestamp(p.create_time())
-    return datetime.now() - creation_time
+    creation_time = datetime.fromtimestamp(p.create_time()).replace(microsecond=0)
+    return datetime.now().replace(microsecond=0) - creation_time
