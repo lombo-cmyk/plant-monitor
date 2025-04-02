5dcd3b4fc98a3fbdaccf4cd04c6448f56bdb6a7a
Lukasz Kania <srsrsrew@gmail.com>
2025-04-02 16:04:09 +0200

    MOCK Dump emails to file instead of logs

--- notification/src/notification/mail/mock/mock.py
+++ notification/src/notification/mail/mock/mock.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pathlib import Path
+
 from notification.mail.abstract_mailbox import AbstractMailbox
 
 
@@ -7,6 +10,14 @@ class MockMailbox(AbstractMailbox):
         pass
 
     def _send(self):
-        self.logger.info(
-            f"MockedMailbox dumping message that would be sent: {self.message_content.model_dump()}"
-        )
+        filename = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.email"
+
+        save_dir = Path("/var/logs/emails")
+        save_dir.mkdir(parents=True, exist_ok=True)
+        save_path = save_dir / Path(filename)
+        with open(save_path, "w") as f:
+            f.write(f"Sender: {self.message_content.sender}\n")
+            f.write(f"Receivers: {self.message_content.receivers}\n")
+            f.write(f"Topic: {self.message_content.topic}\n")
+            f.write(f"Content: \n{self.message_content.content}\n")
+        self.logger.info(f"Message {filename} saved.")
